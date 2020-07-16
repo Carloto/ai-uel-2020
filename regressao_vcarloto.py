4905e8e23557d360bec50ff75d160977bfd28e6f
Carloto <viniciuscarloto@gmail.com>
2020-07-16 14:04:34 -0300

    Fixed subplots

--- regressao_vcarloto.py
+++ regressao_vcarloto.py
@@ -1,18 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def func(XX, value, arr):
     j = 0
     ret = 0
-    # ret = arr[value]
     while j <= value:
         ret = ret + (arr[j]*(XX**(value-j)))
-        j+=1
+        j += 1
 
-    return ret 
+    return ret
 
-min_order = 1 # Grau mínimo do polinômio
-max_order = 10 # Grau máximo do polinômio
+
+min_order = 5  # Grau mínimo do polinômio
+max_order = 10  # Grau máximo do polinômio
 
 lim, npts = 10, 10
 
@@ -23,6 +24,10 @@ pts = plt.ginput(npts)
 plt.close()
 
 pts = np.array(pts)
+fig, axs = plt.subplots(-(-(max_order - (min_order-1))//2), 2)
+fig.set_size_inches(11,10)
+n_subplot = 0
+col = 0
 
 
 x = pts[:, 0]
@@ -31,15 +36,15 @@ y = pts[:, 1]
 order = 1
 poly_list = []
 while order < min_order:
-    poly_list.insert(0,x**order)
-    order +=1 
+    poly_list.insert(0, x**order)
+    order += 1
 
 while order <= max_order:
-    poly_list.insert(0,x**order)
+    poly_list.insert(0, x**order)
     polynomial = np.array(poly_list)
 
     A = np.array([np.ones(len(x))])
-    A = np.concatenate((polynomial,A)).T  # Regressão 
+    A = np.concatenate((polynomial, A)).T  # Regressão
 
     m = np.matmul(np.matmul(np.linalg.inv(np.matmul(A.T, A)), A.T), y)
     print(m)
@@ -47,13 +52,20 @@ while order <= max_order:
     X = np.linspace(-lim, lim, 200)
     Y = func(X, order, m)
 
-    plt.figure(order)
     plt.xlim(-lim, lim)
     plt.ylim(-lim, lim)
-    plt.plot(x, y, "+")
-    plt.plot(X, Y)
-    plt.grid()      
+    axs[n_subplot, col].plot(x, y, "+")
+    axs[n_subplot, col].plot(X, Y)
+    axs[n_subplot, col].grid()
+    axs[n_subplot, col].set_title("Grau " + str(order))
+
+    if col == 0:
+        col = 1
+    else:
+        col = 0
+        n_subplot += 1
 
     order += 1
 
+plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
 plt.show()
